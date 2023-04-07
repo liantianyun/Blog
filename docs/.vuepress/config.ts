126d090d96f88b838ce76196ef9431809704e72a
练天运 <874156304@qq.com>
2023-04-08 01:26:05 +0800

    upd

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -18,5 +18,48 @@ export default defineUserConfig ({
         link: '/books',
       },
     ],
+    sidebar: {
+      '/books': [
+        {
+          text: '阅读',
+          link: '/books',
+        }
+      ],
+      '/diary': [
+        {
+          text: '2023年',
+          collapsible: true,
+          children: [
+            {
+              text: '三月',
+              collapsible: true,
+              children: [
+                '/diary/2023/03/30',
+              ]
+            },
+            {
+              text: '四月',
+              collapsible: true,
+              children: [
+                '/diary/2023/04/01',
+                '/diary/2023/04/02',
+                '/diary/2023/04/03',
+                '/diary/2023/04/04',
+                '/diary/2023/04/05',
+                '/diary/2023/04/06',
+                '/diary/2023/04/07',
+              ]
+            }
+          ]
+        },
+      ],
+      '/': [],
+    },
+    editLink: false,
+    notFound: ['页面不存在'],
+    backToHome: '回到首页',
+    contributors: false,
+    lastUpdatedText: '编辑时间',
+    repo: 'https://github.com/liantianyun/Blog',
   })
 });
