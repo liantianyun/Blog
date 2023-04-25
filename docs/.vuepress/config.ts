a74c14bd3ee99e109dade40029a4b9c3d2c63113
练天运 <874156304@qq.com>
2023-04-25 22:34:41 +0800

    upd

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -150,6 +150,7 @@ export default defineUserConfig ({
                 '/diary/2023/04/21',
                 '/diary/2023/04/22',
                 '/diary/2023/04/23',
+                '/diary/2023/04/24',
               ]
             }
           ]
